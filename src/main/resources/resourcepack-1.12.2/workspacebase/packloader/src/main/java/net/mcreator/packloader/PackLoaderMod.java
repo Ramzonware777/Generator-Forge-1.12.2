598c67d7cf9eede1ce154d8c0cfe93e0f8260cf2
Spectrall <105946758+Spectrall368@users.noreply.github.com>
2026-01-26 23:02:09 +0100

    Updated pack loader

--- src/main/resources/resourcepack-1.12.2/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
+++ src/main/resources/resourcepack-1.12.2/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
@@ -1,47 +1,54 @@
 package net.mcreator.packloader;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.ResourcePackRepository;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-@Mod(modid = PackLoaderMod.MODID, clientSideOnly = true)
+@Mod(modid = PackLoaderMod.MODID, name = PackLoaderMod.MODID, version = "1.0.0", clientSideOnly = true)
 public class PackLoaderMod {
 
     public static final String MODID = "packloader";
 
-    @Mod.EventBusSubscriber(modid = MODID, value = Side.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLPreInitializationEvent event) {
-            /*List<String> resourcePacks = new ArrayList<>();
-            Path resourcePacksPath = FMLPaths.getOrCreateGameRelativePath(Paths.get("resourcepacks"), "resourcepacks");
-            if (resourcePacksPath.toFile().exists()) {
-                File[] files = resourcePacksPath.toFile().listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        resourcePacks.add(file.getName());
-                    }
+    @Mod.EventHandler @SideOnly(Side.CLIENT)
+    public void preInit(FMLPreInitializationEvent event) {
+        List<String> resourcePacks = new ArrayList<>();
+        Minecraft mc = Minecraft.getMinecraft();
+        File resourcePacksDir = new File(mc.mcDataDir, "resourcepacks");
+
+        if (resourcePacksDir.exists() && resourcePacksDir.isDirectory()) {
+            File[] files = resourcePacksDir.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    resourcePacks.add(file.getName());
                 }
             }
+        }
 
-            boolean anyChanged = false;
-            Collection<ResourcePackRepository.Entry> selectedPacks = new LinkedList<>(Minecraft.getMinecraft().getResourcePackRepository().getRepositoryEntries());
-            Collection<ResourcePackRepository.Entry> allPacks = Minecraft.getMinecraft().getResourcePackRepository().getRepositoryEntriesAll();
-            for (ClientResourcePackInfo pack : allPacks) {
-                for (String resourcePack : resourcePacks) {
-                    if (pack.getResourcePackName().contains(resourcePack)) {
+        boolean anyChanged = false;
+        ResourcePackRepository repo = mc.getResourcePackRepository();
+        List<ResourcePackRepository.Entry> selectedPacks = new ArrayList<>(repo.getRepositoryEntries());
+
+        for (ResourcePackRepository.Entry entry : repo.getRepositoryEntriesAll()) {
+            for (String resourcePack : resourcePacks) {
+                if (entry.getResourcePackName().contains(resourcePack)) {
+                    if (!selectedPacks.contains(entry)) {
                         anyChanged = true;
-                        selectedPacks.add(pack);
+                        selectedPacks.add(entry);
                     }
                 }
             }
+        }
 
-            if (anyChanged) {
-                Minecraft.getMinecraft().getResourcePackRepository().setSelected(selectedPacks);
-                Minecraft.getMinecraft().gameSettings.loadResourcePacks(Minecraft.getMinecraft().getResourcePackRepository());
-            }*/
+        if (anyChanged) {
+            repo.setRepositories(selectedPacks);
+            repo.updateRepositoryEntriesAll();
         }
     }
 }
